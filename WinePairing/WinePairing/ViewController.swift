30ec8a17912d66f7e5753b46dd5be6c91b80bbdd
Jacqueline Alves <jacque.alves.b@gmail.com>
2020-12-10 21:14:47 -0300

    Project setup

---
+++ WinePairing/WinePairing/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WinePairing
+//
+//  Created by Jacqueline Alves on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
