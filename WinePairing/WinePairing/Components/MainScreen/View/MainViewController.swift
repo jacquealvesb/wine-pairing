09cd9f46924ab12bc08f72d5d4a884803002d92f
Jacqueline Alves <jacque.alves.b@gmail.com>
2021-01-19 11:09:14 -0300

    Implements winr pairing search

--- WinePairing/WinePairing/Components/MainScreen/View/MainViewController.swift
+++ WinePairing/WinePairing/Components/MainScreen/View/MainViewController.swift
@@ -9,14 +9,90 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    let screen = MainView(frame: UIScreen.main.bounds)
+    // MARK: - Views
+    
+    let contentView = MainView(frame: UIScreen.main.bounds)
+    
+    // MARK: - Variables
+    
+    var dataProvider: DataProvidable?
+    
+    // MARK: - Life cycle
     
     override func loadView() {
-        self.view = screen
+        self.view = contentView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupDataProvider()
+        setupSearchButton()
+        setupSeachBar()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupDataProvider() {
+        dataProvider = DataProvider(service: AlmofireService.shared, apiKey: kSpoonacularApiKey)
+    }
+    
+    private func setupSearchButton() {
+        contentView.pairButton.action = { [weak self] in
+            guard let `self` = self,
+                  let food = self.contentView.searchBar.text, !food.isEmpty else { return } // TODO: Error message
+            self.searchWinePairing(with: food)
+        }
+    }
+    
+    private func setupSeachBar() {
+        contentView.searchBar.delegate = self
+    }
+    
+    // MARK: - Functions
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    private func searchWinePairing(with food: String) {
+        contentView.activityIndicator.startAnimating()
+        
+        dataProvider?.fetchPair(for: food) { [weak self] result in
+            guard let `self` = self else { return }
+            self.contentView.activityIndicator.stopAnimating()
+            switch result {
+            case .failure(let error):
+                print(error)
+                self.showEmptyResults(searchedText: food)
+            case .success(let pairing):
+                self.showPairingResults(pairing)
+            }
+        }
+    }
+    
+    private func showPairingResults(_ pairing: WinePairing) {
+        let viewController = PairingViewController()
+        viewController.viewModel = PairingViewModel(state: .pairing(pairing))
+        
+        present(viewController, animated: true)
+    }
+    
+    private func showEmptyResults(searchedText: String) {
+        let viewController = PairingViewController()
+        viewController.viewModel = PairingViewModel(state: .empty(searchedText))
+        
+        present(viewController, animated: true)
+    }
+}
+
+extension MainViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let food = searchBar.text, !food.isEmpty else { return } // TODO: Error message
+        searchWinePairing(with: food)
+        
+        searchBar.text = ""
+        searchBar.endEditing(true)
     }
 }
